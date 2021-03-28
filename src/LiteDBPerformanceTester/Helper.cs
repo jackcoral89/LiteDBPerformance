89426f18ea73f214699694a8f0dbceeb2f97e141
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2021-03-28 21:59:08 +0200

    add NLipsum

--- src/LiteDBPerformanceTester/Helper.cs
+++ src/LiteDBPerformanceTester/Helper.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLipsum.Core;
 
 namespace LiteDBPerformanceTester
 {
@@ -33,56 +34,23 @@ namespace LiteDBPerformanceTester
                 yield return new TestDoc
                 {
                     Name = Guid.NewGuid().ToString(),
-                    LongText = LoremIpsum(3,10,2,10,5)
+                    LongText = LoremIpsum()
                 };
             }
         }
 
-        private static string LoremIpsum(int minWords, int maxWords, int minSentences, int maxSentences,
-            int numParagraphs)
+        private static string LoremIpsum()
         {
-            var words = new[]
-            {
-                "lorem", "ipsum", "dolor", "sit", "amet", "consectetuer",
-                "adipiscing", "elit", "sed", "diam", "nonummy", "nibh", "euismod",
-                "tincidunt", "ut", "laoreet", "dolore", "magna", "aliquam", "erat"
-            };
-
-            var rand = new Random(DateTime.Now.Millisecond);
-            var numSentences = rand.Next(maxSentences - minSentences) + minSentences + 1;
-            var numWords = rand.Next(maxWords - minWords) + minWords + 1;
-
-            var result = new StringBuilder();
-
-            for (int p = 0; p < numParagraphs; p++)
-            {
-                for (int s = 0; s < numSentences; s++)
-                {
-                    for (int w = 0; w < numWords; w++)
-                    {
-                        if (w > 0)
-                        {
-                            result.Append(" ");
-                        }
-
-                        result.Append(words[rand.Next(words.Length)]);
-                    }
-
-                    result.Append(". ");
-                }
-
-                result.AppendLine();
-            }
-
-            return result.ToString();
+            var lipsum = new LipsumGenerator();
+            return lipsum.GenerateLipsum(1);
         }
 
         public class TestDoc
         {
             public int Id { get; set; }
-            
+
             public string Name { get; set; }
-            
+
             public string LongText { get; set; }
         }
     }
